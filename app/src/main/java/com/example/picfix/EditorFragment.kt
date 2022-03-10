afaf4cccb8986571e1d07d20a4e2093a29d8927e
Jonni <enterthesource@me.com>
2022-03-10 14:39:22 +0100

    Added room db

--- app/src/main/java/com/example/picfix/EditorFragment.kt
+++ app/src/main/java/com/example/picfix/EditorFragment.kt
@@ -4,23 +4,61 @@ import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.picfix.databinding.EditorFragmentBinding
 
 class EditorFragment : Fragment() {
 
     private lateinit var viewModel: EditorViewModel
+    private val args: EditorFragmentArgs by navArgs()
+    private lateinit var binding: EditorFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.editor_fragment, container, false)
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_check)
+        }
+        setHasOptionsMenu(true)
+
+        binding = EditorFragmentBinding.inflate(inflater, container, false)
+        binding.editor.setText("You selected note number ${args.noteId}")
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    saveAndReturn()
+                }
+            }
+        )
+        return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> saveAndReturn()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveAndReturn(): Boolean {
+        findNavController().navigateUp()
+        return true
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(EditorViewModel::class.java)
     }
-
 }
--- app/src/main/java/com/example/picfix/data/NoteEntity.kt
+++ app/src/main/java/com/example/picfix/data/NoteEntity.kt
@@ -1,9 +1,13 @@
 package com.example.picfix.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.picfix.NEW_NOTE_ID
 import java.util.*
 
+@Entity(tableName = "notes")
 data class NoteEntity (
+    @PrimaryKey(autoGenerate = true)
     var id: Int,
     var date: Date,
     var text: String
